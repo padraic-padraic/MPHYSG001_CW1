dc6a4b245ccd99e533b42b5b3d864236619fbff6
Padraic Calpin <padraic.calpin93@gmail.com>
2016-01-10 13:28:50 +0000

    Modified Setup Add testing suite and my details

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setup(
     name = "Greengraph",
     version = "1.0",
     packages = find_packages(exclude=['*test']),
+    author = "Padraic Calpin",
+    author_email = "padraic.calpin93@gmail.com",
+    license = "GPLv2",
     scripts = ['scripts/greengraph'],
-    install_requires = ['requests','numpy','matplotlib','geopy'])
+    install_requires = ['requests','numpy','matplotlib','geopy'],
+    setup_requires = ['nose'],
+    test_suite = 'nose.collector')
